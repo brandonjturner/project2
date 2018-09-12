8c40fc3fcc91aaba361b06ac3b54d0d9bdecd7a9
Brucimus <Brucimus@gmail.com>
2018-09-11 23:13:12 -0700

    created models so no errors upon node server launch

--- models/carpool.js
+++ models/carpool.js
@@ -1,28 +1,26 @@
 module.exports = function(sequelize, DataTypes) {
-    var Carpool = sequelize.define("Carpool", {
-      origin: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      destination: {
-        type: DataTypes.String,
+  var Carpool = sequelize.define("Carpool", {
+    origin: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    destination: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    departureTime: {
+      type: DataTypes.TIME,
+      allowNull: false
+    }
+  });
+  Carpool.associate = function(models) {
+    // We're saying that a Carpool should belong to an People
+    // A Carpool can't be created without an People due to the foreign key constraint
+    Carpool.belongsTo(models.People, {
+      foreignKey: {
         allowNull: false
-      },
-      passengers: {
-          type: DataTypes.ARRAY(DataTypes.JSON),
-          allowNull: true
       }
     });
-
-    Carpool.associate = function(models) {
-        // We're saying that a Carpool should belong to an People
-        // A Carpool can't be created without an People due to the foreign key constraint
-        Carpool.belongsTo(models.People, {
-          foreignKey: {
-            allowNull: false
-          }
-        });
-    };
-  
-    return Carpool;
   };
+  return Carpool;
+};

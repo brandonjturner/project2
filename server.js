9deead22403b63c2f60b5b0470ec128279794e2e
Cyrus Sepahbodi <cyrussepahbodi@yahoo.com>
2018-09-13 20:16:26 -0700

    this commit includes the initial setup for bcrypt and password hashing modules

--- server.js
+++ server.js
@@ -6,10 +6,12 @@ var exphbs = require("express-handlebars");
 var db = require("./models");
 var app = express();
 var PORT = process.env.PORT || 3000;
+
 // Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static("public"));
+
 // Handlebars
 app.engine(
   "handlebars",
@@ -18,16 +20,19 @@ app.engine(
   })
 );
 app.set("view engine", "handlebars");
+
 // Routes
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
 var syncOptions = { force: false };
+
 // If running a test, set syncOptions.force to true
 // clearing the `testdb`
 if (process.env.NODE_ENV === "test") {
   syncOptions.force = true;
 }
+
 // Starting the server, syncing our models ------------------------------------/
 db.sequelize.sync(syncOptions).then(function() {
   app.listen(PORT, function() {

668a712380d5ad0d0c004142da16a6788d1b9801
Cyrus Sepahbodi <cyrussepahbodi@yahoo.com>
2018-09-15 10:31:59 -0700

    adding passport authenticate logic

--- Controllers/login.js
+++ Controllers/login.js
@@ -1,41 +1,16 @@
-//passport.js dependency
-
-var passport = require('passport')
-  , LocalStrategy = require('passport-local').Strategy;
-
-//local strategy for username and password login authentication
-
 passport.use(new LocalStrategy(
-  function(username, password, done) {
-    User.findOne({ username: username }, function(err, user) {
-      if (err) { return done(err); }
-      if (!user) {
-        return done(null, false, { message: 'Incorrect username.' });
-      }
-      if (!user.validPassword(password)) {
-        return done(null, false, { message: 'Incorrect password.' });
-      }
-      return done(null, user);
-    });
-  }
-));
+    function(username, password, done) {
+        User.findOne({ username: username }, function (err, user) {
+            if (err) { return done(err); }
+            if (!user) { return done(null, false); }
+            if (!user.verifyPassword(password)) { return done(null, false); }
+            return done(null, user);
+        });
+    }
+  ));
 
-//passport.js dependency for FB login
-
-var passport = require('passport')
-  , FacebookStrategy = require('passport-facebook').Strategy;
-
-//facebook login authentication strategy
-
-passport.use(new FacebookStrategy({
-    clientID: FACEBOOK_APP_ID,
-    clientSecret: FACEBOOK_APP_SECRET,
-    callbackURL: "http://www.example.com/auth/facebook/callback"
-  },
-  function(accessToken, refreshToken, profile, done) {
-    User.findOrCreate(..., function(err, user) {
-      if (err) { return done(err); }
-      done(null, user);
-    });
-  }
-));
+  app.post('/login', 
+  passport.authenticate('local', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  });

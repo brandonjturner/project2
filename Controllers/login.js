081a4969550cabbb64777af802626dd1d8aebc15
Cyrus Sepahbodi <cyrussepahbodi@yahoo.com>
2018-09-15 09:51:42 -0700

    this commit includes facebook authentication

--- Controllers/login.js
+++ Controllers/login.js
@@ -20,3 +20,22 @@ passport.use(new LocalStrategy(
   }
 ));
 
+//passport.js dependency for FB login
+
+var passport = require('passport')
+  , FacebookStrategy = require('passport-facebook').Strategy;
+
+//facebook login authentication strategy
+
+passport.use(new FacebookStrategy({
+    clientID: FACEBOOK_APP_ID,
+    clientSecret: FACEBOOK_APP_SECRET,
+    callbackURL: "http://www.example.com/auth/facebook/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    User.findOrCreate(..., function(err, user) {
+      if (err) { return done(err); }
+      done(null, user);
+    });
+  }
+));

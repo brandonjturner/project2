4568190f6c5587c3ce33dc85d46ffe78ae9318e9
Cyrus Sepahbodi <cyrussepahbodi@yahoo.com>
2018-09-15 09:37:00 -0700

    this commit includes the controllers folder and the beginning of the login logic

---
+++ Controllers/login.js
@@ -0,0 +1,22 @@
+//passport.js dependency
+
+var passport = require('passport')
+  , LocalStrategy = require('passport-local').Strategy;
+
+//local strategy for username and password login authentication
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.findOne({ username: username }, function(err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  }
+));
+
